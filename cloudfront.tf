6fedc584c426306305554cca2c44fbb87fdbcb41
Krishan Shamod <krishanshamod@gmail.com>
2023-07-26 04:55:05 +0530

    Updated cloudfront security configs

--- cloudfront.tf
+++ cloudfront.tf
@@ -30,7 +30,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 0
     max_ttl                = 86400
